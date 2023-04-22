e83efe9ffc0efaac6e1a7bf138f8c50e98950914
TejasPrabhu21 <tejasprabhu21@gmail.com>
2023-04-22 19:26:57 +0530

    cloned repo

---
+++ contact.js
@@ -0,0 +1,25 @@
+////////////Contact me section smtp implementation
+const namess = document.querySelector('#names');
+
+var names = namess.val();
+var email = $('#email').val();
+var phone = $('#phone').val();
+var subject = $('#subject').val();
+var message = $('#message').val();
+
+console.log(names,email,phone,subject,message);
+function sendEmail() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "username",
+        Password: "password",
+        To: 'them@website.com',
+        From: "you@isp.com",
+        Subject: "This is the subject",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
+
+window.alert("Message Sent");
---
+++ script.js
@@ -0,0 +1,57 @@
+/////////////Navbar hover effect
+const nav = document.querySelector('.navbar');
+
+const handlehover = function (e, opacity) {
+  if (e.target.classList.contains('nav_item')) {
+    const link = e.target;
+    const siblings = link.closest('.navbar').querySelectorAll('.nav_item');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  handlehover(e, 0.6);
+});
+
+nav.addEventListener('mouseout', function (e) {
+  handlehover(e, 1);
+});
+
+
+///////////////Hero Section on load animation
+const section1 = document.querySelector('.hero');
+
+const revealhero = function(entries,observer1){
+    const [entry] = entries;
+    if(!entry.isIntersecting) return;
+    entry.target.classList.remove('hero--initial');
+    observer1.unobserve(entry.target);
+}
+
+const heroobserver = new IntersectionObserver(revealhero,{root:null,threshold:0.2,});
+
+heroobserver.observe(section1);
+section1.classList.add('hero--initial');
+
+
+//////Revealing sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null, threshold: 0.2,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
